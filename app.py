f36900d8c8ea5dee071c5f86f7c7a3b71e34b7e3
Rafael Toschi Chiafarelli <rafael.chiafarelli@gmail.com>
2022-07-15 19:34:20 -0300

    arquivos modificados para contemplar o serviço de controle do robo

--- app.py
+++ app.py
@@ -4,15 +4,24 @@ Create a webpage that is constantly updated with random numbers from a backgroun
 
 # Start with a basic flask app webpage.
 from tkinter.font import names
+from tokenize import Single
 from turtle import forward
 from flask_socketio import SocketIO, emit
-from flask import Flask, render_template, url_for, copy_current_request_context
+from flask import Flask, render_template, url_for, copy_current_request_context, jsonify
 from random import random
 from time import sleep
 from threading import Thread, Event
 from random import randint
 import serial,json
 from model import limits, steering, movement
+import serial.tools.list_ports as ports
+import cv2 
+from serial_port import Singleton
+import tkinter as tk
+import subprocess
+
+from PIL import Image
+
 __author__ = 'Eremita'
 
 app = Flask(__name__)
@@ -25,34 +34,10 @@ socketio = SocketIO(app,async_mode = "threading",logging = False, engineio_logge
 #random number Generator Thread
 thread = Thread()
 thread_stop_event = Event()
+                
+serialPort = Singleton()
 
-#serial comunication thread
-s_thread = Thread()
-serial_stop_event = Event()
 
-ser = serial.Serial('/dev/ttyUSB0', 38400)
-count = 0
-
-class RandomThread(Thread):
-    def __init__(self):
-        self.delay = 1
-        super(RandomThread, self).__init__()
-
-    def randomNumberGenerator(self):
-        """
-        Generate a random number every 1 second and emit to a socketio instance (broadcast)
-        Ideally to be run in a separate thread?
-        """
-        #infinite loop of magical random numbers
-        print("Making random numbers")
-        while not thread_stop_event.isSet():
-            number = randint(11111, 99999)
-            #print(number)
-            socketio.emit('newnumber', {'number': number}, namespace='/test')
-            sleep(self.delay)
-
-    def run(self):
-        self.randomNumberGenerator()
 
 
 @app.route('/')
@@ -67,73 +52,44 @@ def admin():
 def dev():
     return render_template('dev.html')
 
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    # need visibility of the global thread object
-    global thread
-    print('Client connected')
-    #Start the random number generator thread only if the thread has not been started before.
-    if not thread.is_alive():
-        print("Starting Thread")
-        thread = RandomThread()
-        thread.start()
+@app.route('/showbat')
+def showbat():
+    #launch app bat 
+    #"C:\\Users\\rafae\\Documents\\workspace\\PoliteaBot\\LowLevelBatt.png"
+    print("***********************************************************")
+    subprocess.call(["C:\\Users\\rafae\\Documents\\workspace\\PoliteaBot\\venv\\Scripts\\python.exe", "C:\\Users\\rafae\\Documents\\workspace\\PoliteaBot\\bat_app.py"])
+    return jsonify({"result":"done"})
 
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
-    serial_stop_event.set()
-
-@socketio.on('stop')
-def stop_connections():
-    serial_stop_event.set()
-    thread_stop_event.set()
+@app.route('/showbat_turnoff')
+def showbat_turnoff():
+    #launch app bat 
+    #"C:\\Users\\rafae\\Documents\\workspace\\PoliteaBot\\LowLevelBatt.png"
+    print("***********************************************************")
+    subprocess.call(["C:\\Users\\rafae\\Documents\\workspace\\PoliteaBot\\venv\\Scripts\\python.exe", "C:\\Users\\rafae\\Documents\\workspace\\PoliteaBot\\timer_test.py"])
+    return jsonify({"result":"done"})
 
 @socketio.on('message', namespace='/test')
 def received_msg(data):
-    global ser
-    global count
-    x = int(data['x'])
-    forward = 0
-    if(x<0):
-        forward = 1
-        x = x*(-1)
-    if x>99:
-        x=99
+    global serialPort
 
+    x = int(data['x'])
     y = int(data['y'])
-    left = 0
-    if(y<0):
-        left = 1
-        y = y*(-1)
-    if y>99:
-        y=99
-    speed_x = limits[x]
-    speed_y = limits[y]
-    command = 0
-    if (speed_y,speed_x,left,forward) in steering:
-        if steering[(speed_y,speed_x,left,forward)] in movement:
-            command = movement[steering[(speed_y,speed_x,left,forward)]]
-    count+=1    
-    if count > 99999999:
-        count = 0    
-    if forward == 1:
-        x  = -1 * x
-    if left == 1:
-        y = -1 * y
+    command = int(data['cmd'])
+
+    serialPort.count+=1    
+    if serialPort.count > 99999999:
+        serialPort.count = 0    
+
 
     mystring = ("{cmd:"+ f'{command:02d}' + 
                 ",t:" + f'{x:+03d}' +
                 ",y:" + f'{y:+03d}' +
-                ",hb:" + f'{count:08d}' + 
+                ",hb:" + f'{serialPort.count:08d}' + 
                 "};").encode("utf-8")
-    print(mystring)
-    ser.write(mystring)
-
-    ser.flush()
-
-
+    print(mystring,flush=True)
+    serialPort.ser.write(mystring)
+    serialPort.ser.flush()
 
 if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000, log_output = True)
     
-    
-    socketio.run(app, host='127.0.0.1', port=5000, log_output = False)

01ba28074e66a488ccbabe38376c69f215adb5c9
Rafael Toschi Chiafarelli <rafael.chiafarelli@gmail.com>
2022-07-17 09:48:45 -0300

    nova versão do teclado. Compilando na workstation

---
+++ VanessaKey/include/key_copdes.h
@@ -0,0 +1,119 @@
+/*
+NAME		CODE	VALUE
+
+#define ZERO 29
+#define ONE	18
+#define TWO 19
+#define THREE 20
+#define FOUR 21
+#define 		5	23
+#define 		6	22
+#define 		7	26
+#define 		8	28
+#define 		9	25
+#define 		A	0
+#define 		B	11
+#define 		C	8
+#define 		D	2
+#define 		E	14
+#define 		F	3
+#define 		G	5
+#define 		H	4
+#define 		I	34
+#define 		J	38
+#define 		K	40
+#define 		L	37
+#define 		M	46
+#define 		N	45
+#define 		O	31
+#define 		P	35
+#define 		Q	12
+#define 		R	15
+#define 		S	1
+#define 		T	17
+#define 		U	32
+#define 		V	9
+#define 		W	13
+#define 		X	7
+#define 		Y	16
+#define 		Z	6
+#define SectionSign	?	10
+#define Grave		`	50
+#define Minus		-	27
+#define Equal		=	24
+#define LeftBracket	[	33
+#define RightBracket	]	30
+#define Semicolon	;	41
+#define Quote		'	39
+#define Comma		","	43
+#define Period		.	47
+#define Slash		/	44
+#define Backslash	\	42
+#define Keypad0		0	82
+#define Keypad1		1	83
+#define Keypad2		2	84
+#define Keypad3		3	85
+#define Keypad4		4	86
+#define Keypad5		5	87
+#define Keypad6		6	88
+#define Keypad7		7	89
+#define Keypad8		8	91
+#define Keypad9		9	92
+#define KeypadDecimal	.	65
+#define KeypadMultiply	*	67
+#define KeypadPlus	+	69
+#define KeypadDivide	/	75
+#define KeypadMinus	-	78
+#define KeypadEquals	=	81
+#define KeypadClear	?	71
+#define KeypadEnter	?	76
+#define Space		?	49
+#define Return		?	36
+#define Tab		?	48
+#define Delete		?	51
+#define ForwardDelete	?	117
+#define Linefeed 	?	52
+#define Escape		?	53
+#define Command		?	55
+#define Shift		?	56
+#define CapsLock	?	57
+#define Option		?	58
+#define Control		?	59
+#define RightShift	?	60
+#define RightOption	?	61
+#define RightControl	?	62
+#define Function	fn	63
+#define 		F1	122
+#define 		F2	120
+#define 		F3	99
+#define 		F4	118
+#define 		F5	96
+#define 		F6	97
+#define 		F7	98
+#define 		F8	100
+#define 		F9	101
+#define 		F10	109
+#define 		F11	103
+#define 		F12	111
+#define 		F13	105
+#define BrightnessDown	F14	107
+#define BrightnessUp	F15	113
+#define 		F16	106
+#define 		F17	64
+#define 		F18	79
+#define 		F19	80
+#define 		F20	90
+#define VolumeUp 	?	72
+#define VolumeDown 	?	73
+#define Mute 		?	74
+#define Help/Insert	?	114
+#define Home		?	115
+#define End		?	119
+#define PageUp		?	116
+#define PageDown	?	121
+*/
+#define LeftArrow	123
+#define RightArrow	124
+#define DownArrow	125
+#define UpArrow 126
+
--- VanessaKey/src/main.cpp
+++ VanessaKey/src/main.cpp
@@ -1,9 +1,81 @@
-#include <Arduino.h>
+#include "Keyboard.h"
+void CheckForKey(int key);
+#define KEY_LOW 1000
+// set pin numbers for the five buttons:
+const int Buttons[] = {8,9,10,16};
+
+
+int btn_count;
 
 void setup() {
-  // put your setup code here, to run once:
+    pinMode(Buttons[0], INPUT_PULLUP);
+    pinMode(Buttons[1], INPUT_PULLUP);
+    pinMode(Buttons[2], INPUT_PULLUP);
+    pinMode(Buttons[3], INPUT_PULLUP);
+
+
+
+    
+    // initialize control over the keyboard:
+    Keyboard.begin();
+    btn_count = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // check for incoming serial data:
+
+  // use the pushbuttons to control the keyboard:
+  if(btn_count >= 0 && btn_count <= 3)
+  {
+    CheckForKey(Buttons[btn_count]);
+  }
+  btn_count += 1;
+  if (btn_count > 3)
+  {
+    btn_count = 0;
+  }
+  
+}
+
+void CheckForKey(int key){
+  long int timecount = 0;
+  if (digitalRead(key) == LOW) {
+    
+    while(digitalRead(key) == LOW && timecount < KEY_LOW) {
+      timecount+=1;
+      delayMicroseconds(100);
+    }
+    if (digitalRead(key) == LOW && timecount >= KEY_LOW)
+    {
+    
+      switch(key)
+      {
+        case 8:
+          Keyboard.press(KEY_LEFT_CTRL);
+          Keyboard.press(KEY_RIGHT_ARROW);   
+        break;
+        case 9:
+          Keyboard.press(KEY_LEFT_CTRL);
+          Keyboard.press(KEY_DOWN_ARROW);
+
+        break;
+        case 10:
+          Keyboard.press(KEY_LEFT_CTRL);
+          Keyboard.press(KEY_UP_ARROW);      
+        break;
+        case 16:
+          Keyboard.press(KEY_LEFT_CTRL);
+          Keyboard.press(KEY_LEFT_ARROW);  
+               
+        break;
+      }
+      delay(2);
+      Keyboard.releaseAll();      
+      
+    }
+  }
+  //hold them here
+  while(digitalRead(key) == LOW ) {
+    delayMicroseconds(100);
+  }
 }
